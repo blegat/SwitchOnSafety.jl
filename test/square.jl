4780cd0d5977f9e10b46f46941c2aebc97fecbaa
Benoît Legat <benoit.legat@gmail.com>
2017-10-11 20:53:32 +0200

    Automatic computation of c with chebyshev centers

--- test/square.jl
+++ test/square.jl
@@ -1,5 +1,5 @@
 @testset "CIS square example with $solver" for solver in blp_solvers
     include(Pkg.dir("HybridSystems", "examples", "square.jl"))
-    p = getis(hs, solver, [[0., 0]])
+    p = getis(hs, solver)
     @test p[1].P ≈ [-1 0 0; 0 1 0; 0 0 1]
 end
