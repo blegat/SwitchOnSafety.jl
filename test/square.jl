25245b38c54ef340ab42367e07b298c2502ddf85
Benoît Legat <benoit.legat@gmail.com>
2017-11-09 00:44:15 +0100

    Fixes

--- test/square.jl
+++ test/square.jl
@@ -1,5 +1,5 @@
 @testset "CIS square example with $solver" for solver in blp_solvers
     include(Pkg.dir("HybridSystems", "examples", "square.jl"))
     p = getis(hs, solver)
-    @test p[1].P ≈ [-1 0 0; 0 1 0; 0 0 1]
+    @test p[1].P ≈ [-1 0 0; 0 1 0; 0 0 1] rtol=1e-5
 end
