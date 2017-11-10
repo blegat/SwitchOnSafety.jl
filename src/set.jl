bbf01dc77229417e321967980b0d30a8a94d5570
Benoît Legat <benoit.legat@gmail.com>
2017-11-10 18:29:18 +0100

    Various improvement

--- src/set.jl
+++ src/set.jl
@@ -12,7 +12,7 @@ end
 
 @recipe function f(ell::Ellipsoid)
     @assert Base.LinAlg.checksquare(ell.Q) == 2
-    αs = linspace(0, 2π, 64)
+    αs = linspace(0, 2π, 1024)
     ps = [[cos(α), sin(α)] for α in αs]
     r = [sqrt(dot(p, ell.Q * p)) for p in ps]
     seriestype --> :shape
@@ -24,6 +24,17 @@ struct LiftedEllipsoid{T}
     P::Matrix{T}
 end
 
+function LiftedEllipsoid(ell::Ellipsoid)
+    md = ell.Q*ell.c
+    δ = ell.c'*md-1
+    d = -md
+    D = ell.Q
+    P = [δ d'
+         d D]
+    LiftedEllipsoid(P)
+end
+
+Base.convert(::Type{Ellipsoid{T}}, ell::LiftedEllipsoid) where T = convert(Ellipsoid{T}, Ellipsoid(ell))
 function Ellipsoid(ell::LiftedEllipsoid)
     # P is
     # λ * [c'Qc-1  -c'Q
@@ -55,21 +66,23 @@ struct ConeLyap{T, P<:AbstractPolynomial{T}, S}
     p::P
     Q::Matrix{S}
     b::Vector{S}
+    β::S
     c::Vector{Float64}
     H::Matrix{Float64}
     #L::Matrix{JuMP.Variable}
     #λinv::Vector{JuMP.Variable}
 end
 
-ConeLyap(p::P, Q::Matrix{S}, b::Vector{S}, c, H) where {T, P<:AbstractPolynomial{T}, S} = ConeLyap{T, P, S}(p, Q, b, c, H)
-JuMP.getvalue(p::ConeLyap) = ConeLyap(getvalue(p.p), getvalue(p.Q), getvalue(p.b), p.c, p.H)
+ConeLyap(p::P, Q::Matrix{S}, b::Vector{S}, β::S, c, H) where {T, P<:AbstractPolynomial{T}, S} = ConeLyap{T, P, S}(p, Q, b, β, c, H)
+JuMP.getvalue(p::ConeLyap) = ConeLyap(getvalue(p.p), getvalue(p.Q), getvalue(p.b), getvalue(p.β), p.c, p.H)
 
 ellipsoid(p::ConeLyap{T, P, JuMP.Variable}) where {T, P<:AbstractPolynomial{T}} = ellipsoid(getvalue(p))
 
+function _HPH(D, d, δ, H)
+    P = [δ d'
+         d D]
+    HPH = H * P * H
+end
 function ellipsoid(p::ConeLyap)
-    n = size(p.Q, 1)
-    P = [-1. p.b'
-         p.b  p.Q]
-    HPH = p.H * P * p.H
-    LiftedEllipsoid(inv(HPH))
+    LiftedEllipsoid(inv(_HPH(p.Q, p.b, p.β, p.H)))
 end
