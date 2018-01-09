ac35f93f47a9083f0a178f6286418057666dd7d5
Cláudio Gomes <cla.gms@gmail.com>
2018-01-09 12:49:28 +0000

    fix calling updatesmp after quickb is computed

---
+++ test/double_msd.jl
@@ -0,0 +1,18 @@
+@testset "Quick bounds update smp" begin
+    
+	Sigma = [eye(4)+1e-4]
+    s = discreteswitchedsystem(Sigma)
+	
+	qlb, qub = quickb(s, 1)
+	
+	@test qlb > 1.
+	
+	@test qub > 1.
+	
+	@test hassmp(s)
+	
+	@test getlb(s) > 1.
+	
+	@test getub(s) > 1.
+	
+end
