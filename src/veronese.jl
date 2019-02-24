427073e2064f2bd2c71c09462148d9221c3ecd17
Benoît Legat <benoit.legat@gmail.com>
2019-02-24 18:11:53 +0100

    ⚡️ Add fast Veronese lift computation

--- src/veronese.jl
+++ src/veronese.jl
@@ -21,10 +21,25 @@ function μ(combi)
         end
     end
     prod *= factorial(cur)
+    return prod
 end
 
-function veroneselift(A::AbstractMatrix, d::Integer)
-    d > 0 || throw(ArgumentError("d should be a positive integer"))
+"""
+   veroneselift_explicit(A::AbstractMatrix, d::Integer)
+
+Computes the Veronese lift of degree `d` of `A` using the explicit formula
+```math
+(A^{[d]})_{\\alpha\\beta} = \\frac{\\text{per}\\,A(\\alpha, \\beta)}{\\sqrt{\\mu(\\alpha)\\mu(\\beta)}}
+```
+given in equation (8) of [PJ08]. Note that this is a lot slower than
+[`veroneselift`](@ref) and is implemented mainly for educational purpose.
+
+* [PJ08] P. Parrilo and A. Jadbabaie.
+*Approximation of the joint spectral radius using sum of squares*.
+Linear Algebra and its Applications, Elsevier, **2008**, 428, 2385-2402
+"""
+function veroneselift_explicit(A::AbstractMatrix, d::Integer)
+    d > 0 || throw(ArgumentError("`d` should be a positive integer, got $d"))
 
     n = LinearAlgebra.checksquare(A)
     N = binomial(n+d-1, d)
@@ -39,5 +54,49 @@ function veroneselift(A::AbstractMatrix, d::Integer)
         end
     end
 
-    Ad
+    return Ad
+end
+
+
+function transform_combination(α, m)
+    β = zeros(Int, m)
+    for i in α
+        β[i] += 1
+    end
+    return β
+end
+
+"""
+   veroneselift(A::AbstractMatrix, d::Integer)
+
+Computes the Veronese lift of degree `d` of `A` using `DynamicPolynomials`.
+"""
+function veroneselift(A::AbstractMatrix, d::Integer)
+    d > 0 || throw(ArgumentError("`d` should be a positive integer, got $d"))
+    m, n = size(A)
+    @polyvar x[1:n]
+    Ax = A * x
+    M = binomial(m + d - 1, d)
+    N = binomial(n + d - 1, d)
+    Ad = Matrix{Float64}(undef, M, N)
+    df = factorial(d)
+    scaling(m) = sqrt(df / prod(factorial, exponents(m)))
+    X = monomials(x, d)
+    # Scaling for the scaled monomial basis
+    col_scaling = Float64[scaling(m) for m in X]
+    if m == n
+        Y = X
+        row_scaling = col_scaling
+    else
+        # Only exponents matter
+        @polyvar y[1:m]
+        Y = monomials(y, d)
+        row_scaling = Float64[scaling(m) for m in Y]
+    end
+    for (i, mono) in enumerate(Y)
+        α = exponents(mono)
+        Axα = prod(i -> Ax[i]^α[i], 1:m)
+        Ad[i, :] = coefficients(Axα, X) .* row_scaling[i] ./ col_scaling
+    end
+    return Ad
 end
--- test/veronese.jl
+++ test/veronese.jl
@@ -2,14 +2,17 @@ using Combinatorics
 
 @testset "Veronese Lift" begin
     @test_throws DimensionMismatch permanent([1 2], collect(Combinatorics.permutations(1:2)))
-    @test_throws DimensionMismatch veroneselift([1 2], 3)
+    @test_throws DimensionMismatch SwitchOnSafety.veroneselift_explicit([1 2], 3)
+    @test_throws ArgumentError SwitchOnSafety.veroneselift_explicit([1 2; 3 4], 0)
     @test_throws ArgumentError veroneselift([1 2; 3 4], 0)
 
-    obtained = veroneselift([11 12; 21 22], 3)
+    @test [1.0 2 * √3 4 * √3 8.0] ≈ @inferred veroneselift([1 2], 3)
+
     s3 = sqrt(3)
     expected = [11^3 s3*11^2*12 s3*11*12^2 12^3;
     s3*11^2*21 11*(11*22+2*21*12) 12*(2*11*22+21*12) s3*12^2*22;
     s3*11*21^2 21*(2*11*22+21*12) 22*(11*22+2*21*12) sqrt(3)*12*22^2;
     21^3 s3*21^2*22 s3*21*22^2 22^3]
-    @test isapprox(obtained, expected)
+    @test expected ≈ @inferred veroneselift([11 12; 21 22], 3)
+    @test expected ≈ @inferred SwitchOnSafety.veroneselift_explicit([11 12; 21 22], 3)
 end
