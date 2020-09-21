fd232998aefdff292871f824fa0519d5ef483d61
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-09-21 09:52:16 +0000

    build based on 43e4c2d

--- dev/generated/AJPR14e54.jl
+++ dev/generated/AJPR14e54.jl
@@ -1,19 +1,25 @@
 using SwitchOnSafety
 A1 = [-1 -1
-      -4 0]
-A2 = [ 3 3
-      -2 1]
+      -4  0]
+A2 = [ 3  3
+      -2  1]
 s = discreteswitchedsystem([A1, A2])
 
 import CSDP
 optimizer_constructor = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true);
 
-soslyapb(s, 1, optimizer_constructor=optimizer_constructor, tol=1e-4)
+lb, ub = soslyapb(s, 1, optimizer_constructor=optimizer_constructor, tol=1e-6, verbose=1)
 
 seq = sosbuildsequence(s, 1, p_0=:Primal)
 psw = findsmp(seq)
 
-soslyapb(s, 2, optimizer_constructor=optimizer_constructor, tol=1e-4)
+lb, ub = soslyapb(s, 2, optimizer_constructor=optimizer_constructor, tol=1e-6, verbose=1)
+
+seq = sosbuildsequence(s, 2, p_0=:Primal)
+psw = findsmp(seq)
+
+G1 = mdependentlift(s, 1, false)
+lb, ub = soslyapb(G1, 1, optimizer_constructor=optimizer_constructor, tol=1e-6, verbose=1)
 
 seq = sosbuildsequence(s, 2, p_0=:Primal)
 psw = findsmp(seq)
